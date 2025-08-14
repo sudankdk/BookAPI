251482d9c62f23f3e0cf91ddbdd09fd7f39b3b1e
sudankdk <sudankhdka345@gmail.com>
2025-08-14 16:48:33 +0545

    sql datbase setup

--- internal/adapter/http/Handlers/routes.go
+++ internal/adapter/http/Handlers/routes.go
@@ -1,13 +1,38 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
 	"github.com/go-chi/chi/v5"
-	inmemorydb "github.com/sudankdk/bookstore/internal/data/Inmemorydb"
+	"github.com/sudankdk/bookstore/internal/data/sqldb"
 	book "github.com/sudankdk/bookstore/internal/domain/usecase/Book"
 )
 
 func Routes() *chi.Mux {
-	repo := inmemorydb.NewInMemoryBookRepo()
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system env variables")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("failed to connect to DB: %v", err)
+	}
+	// repo := inmemorydb.NewInMemoryBookRepo(db)
+	repo := sqldb.NewSqlBookRepo(db)
 	service := book.NewBookHandler(repo)
 	handler := NewBookHandler(service)
 
